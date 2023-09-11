ccae4dd972fe29c6062a8261a56aaef9eff5a0db
johntech-dev <johnayomide50@gmail.com>
2023-09-11 11:50:58 +0100

    j

---
+++ App.js
@@ -0,0 +1,10 @@
+const day = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+
+const myTime = function(){
+  const now = Date.now();
+  document.querySelector('[data-testid="currentUTCTime"]').innerHTML = now;
+};
+
+setInterval(myTime,1000);
+
+day.innerHTML = new Date().toLocaleString('en-US', {weekday: 'long'});
